b786dbd69ece7c6855da8c1a2cb812238f1cc842
Lukasz Folwarczyk <l.folwarczyk@gmail.com>
2017-07-02 21:43:30 +0200

    Very first commit

---
+++ R7C6/src/R7C6.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : R7C6.cpp
+// Author      : £ukasz Folwarczyk
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
