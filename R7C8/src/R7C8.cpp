534bbd4882b353fe567382c515500a4a55480e83
Lukasz Folwarczyk <l.folwarczyk@gmail.com>
2017-07-04 21:13:51 +0200

    R7C8

---
+++ R7C8/src/R7C8.cpp
@@ -0,0 +1,20 @@
+//============================================================================
+// Name        : R7C8.cpp
+// Author      : £ukasz Folwarczyk
+// Version     :
+// Copyright   : Spierdalaj
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+
+	return 0;
+}
