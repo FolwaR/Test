0b51db051655481d57ece4fb4e1c32335b49b3c1
Lukasz Folwarczyk <l.folwarczyk@gmail.com>
2017-07-05 21:39:48 +0200

    R7C10 last commit

--- R7C10/src/R7C10.cpp
+++ R7C10/src/R7C10.cpp
@@ -9,7 +9,42 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+
+double add(double, double);
+double mul(double, double);
+double dif(double, double);
+double sub(double, double);
+
+double (*cal[])(double a, double b) = {add,sub,dif,mul};
+
+
+int main()
+{
+	double a = 10, b = 5;
+
+	for (int i = 0; i < 4; i++)
+		cout << cal[i](a,b) <<endl;
+
 	return 0;
 }
+
+double add(double a, double b)
+{
+	return a+b;
+}
+
+double mul(double a, double b)
+{
+	return a*b;
+}
+
+double dif(double a, double b)
+{
+	return a/b;
+}
+
+double sub(double a, double b)
+{
+	return a-b;
+}
+
